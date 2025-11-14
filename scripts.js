9f11ceeb3bd857efe718d674054159111e7234b0
aakilahamed2005 <aakilahamed2005@gmail.com>
2025-11-14 11:00:44 +0530

    remove checkbox filtering

--- scripts.js
+++ scripts.js
@@ -7,23 +7,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         const checkedItems = document.querySelectorAll('input[name="filter_selection"]:checked');
         const selection = Array.from(checkedItems).map(item => item.value);
 
-
-        if(!selection.length == 0){
-            for(let i=0; i<selection.length; i++){
-                if(selection[i] == "veg"){
-                    isSelect["veg"] = true;
-                    document.querySelector(".non_veg_item").style.display = none;
-                }else if(selection[i] == "non_veg"){
-                    isSelect["non_veg"] = true;
-                }else if(selection[i] == "lunch_special"){
-                    isSelect["non_veg"] = true;
-                }else if(selection[i] == "dinner_special"){
-                    isSelect["non_veg"] = true;
-                }else{
-                    isSelect["show_all"] = true;
-                }
-            }
-        }
+        
     });
 });
 
